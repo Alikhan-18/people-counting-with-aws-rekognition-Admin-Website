4e99cc9157b623cd463fe928e1328d0d4cc0fed0
Arman <zhakypbekov.arman@gmail.com>
2021-04-13 13:55:32 -0700

    save

--- amplify/backend/function/iotFunction/src/index.py
+++ amplify/backend/function/iotFunction/src/index.py
@@ -2,10 +2,11 @@ def handler(event, context):
   print('received event:')
   print(event)
   response = {
-          'statusCode': 200,
-          'headers': {
-              'Access-Control-Allow-Origin': '*'
-          },
-          'body': json.dumps({'message': 'CORS enabled')
-      }
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Credentials": True,
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": "Hello",
+  }
   return response
